1aaa2655e0b5910d35209fc8d1bb78a71aa42452
RuliLG <RuliLG@users.noreply.github.com>
2024-10-14 16:04:33 +0000

    Fix styling

--- src/Facades/LLMPort.php
+++ src/Facades/LLMPort.php
@@ -6,9 +6,9 @@ use Borah\LLMPort\Drivers\LlmProvider;
 use Borah\LLMPort\ValueObjects\ChatRequest;
 use Borah\LLMPort\ValueObjects\ChatResponse;
 use Borah\LLMPort\ValueObjects\LlmModel;
+use Closure;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
-use Closure;
 
 /**
  * @method static void register(string $driver, string $provider)
@@ -19,6 +19,7 @@ use Closure;
  * @method static ChatResponse chat(ChatRequest $request)
  * @method static Collection models()
  * @method static ChatResponse chatStream(ChatRequest $request, Closure $onOutput): ChatResponse;
+ *
  * @see \Borah\LLMPort\LLMPort
  */
 class LLMPort extends Facade
--- tests/FacadeTest.php
+++ tests/FacadeTest.php
@@ -13,51 +13,51 @@ use Borah\LLMPort\ValueObjects\ChatRequest;
 use Borah\LLMPort\ValueObjects\ResponseUsage;
 
 it('can register a provider', function () {
-  expect(LLMPort::driver('openai'))->toBeInstanceOf(OpenAI::class);
+    expect(LLMPort::driver('openai'))->toBeInstanceOf(OpenAI::class);
 
-  LLMPort::register('openai', Groq::class);
+    LLMPort::register('openai', Groq::class);
 
-  expect(LLMPort::driver('openai'))->toBeInstanceOf(Groq::class);
+    expect(LLMPort::driver('openai'))->toBeInstanceOf(Groq::class);
 
-  LLMPort::unregister('openai');
+    LLMPort::unregister('openai');
 
-  expect(LLMPort::driver('openai'))->toBeInstanceOf(OpenAI::class);
+    expect(LLMPort::driver('openai'))->toBeInstanceOf(OpenAI::class);
 });
 
 it('can instantiate Anthropic', function () {
-  expect(LLMPort::driver('anthropic'))->toBeInstanceOf(Anthropic::class);
+    expect(LLMPort::driver('anthropic'))->toBeInstanceOf(Anthropic::class);
 });
 
 it('can instantiate Nebius', function () {
-  expect(LLMPort::driver('nebius'))->toBeInstanceOf(Nebius::class);
+    expect(LLMPort::driver('nebius'))->toBeInstanceOf(Nebius::class);
 });
 
 it('can instantiate Replicate', function () {
-  expect(LLMPort::driver('replicate'))->toBeInstanceOf(Replicate::class);
+    expect(LLMPort::driver('replicate'))->toBeInstanceOf(Replicate::class);
 });
 
 it('can instantiate OpenAI', function () {
-  expect(LLMPort::driver('openai'))->toBeInstanceOf(OpenAI::class);
+    expect(LLMPort::driver('openai'))->toBeInstanceOf(OpenAI::class);
 });
 
 it('can instantiate Groq', function () {
-  expect(LLMPort::driver('groq'))->toBeInstanceOf(Groq::class);
+    expect(LLMPort::driver('groq'))->toBeInstanceOf(Groq::class);
 });
 
 it('can instantiate Gemini', function () {
-  expect(LLMPort::driver('gemini'))->toBeInstanceOf(Gemini::class);
+    expect(LLMPort::driver('gemini'))->toBeInstanceOf(Gemini::class);
 });
 
 it('can be used without specific driver', function () {
-  $response = LLMPort::chat(new ChatRequest(
-    messages: [
-        new ChatMessage(role: MessageRole::System, content: 'You are an AI assistant that just replies with Yes or No'),
-        new ChatMessage(role: MessageRole::User, content: 'Are you an AI model?'),
-    ]
-  ));
+    $response = LLMPort::chat(new ChatRequest(
+        messages: [
+            new ChatMessage(role: MessageRole::System, content: 'You are an AI assistant that just replies with Yes or No'),
+            new ChatMessage(role: MessageRole::User, content: 'Are you an AI model?'),
+        ]
+    ));
 
-  expect($response->content)->toContain('Yes');
-  expect($response->usage)->toBeInstanceOf(ResponseUsage::class);
-  expect($response->usage->inputTokens)->toBeInt()->toBeGreaterThan(0);
-  expect($response->usage->outputTokens)->toBeInt()->toBeGreaterThan(0);
+    expect($response->content)->toContain('Yes');
+    expect($response->usage)->toBeInstanceOf(ResponseUsage::class);
+    expect($response->usage->inputTokens)->toBeInt()->toBeGreaterThan(0);
+    expect($response->usage->outputTokens)->toBeInt()->toBeGreaterThan(0);
 });
